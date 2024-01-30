0420c93f3cbd9c0c6cce1dc2ef30725c89d3c299
Bradley Miller <bradley.w.miller@gmail.com>
2024-01-29 21:57:11 -0600

    Updated to fix upload issue?

--- resources/views/livewire/inquiry.blade.php
+++ resources/views/livewire/inquiry.blade.php
@@ -1,12 +1,12 @@
 <div>
-    <form wire:submit="create">
+    <form wire:submit="create" class="text-gray-900 dark:text-stone-50">
         {{ $this->form }}
-        
+
         <div>
             <button type="submit" class="mt-10 inline-flex justify-center rounded-md border border-transparent bg-green-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">Submit</button>
           </div>
     </form>
-    
+
     <x-filament-actions::modals />
 </div>
 
